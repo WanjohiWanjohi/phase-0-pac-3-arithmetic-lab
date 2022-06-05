99880824725cf915b33f928748b3d2d4ad2cf182
WanjohiWanjohi <wamuyuwanjohi97@gmail.com>
2022-06-05 21:51:23 +0300

    commit tree 841a824fc39624afeaaf2b850bbb9a3c9b94b447

---
+++ index.js
@@ -0,0 +1,28 @@
+function add(a , b) {
+    return a+b;
+  }
+  
+  function subtract(a ,b) {
+    // we'll fill this in shortly
+    return a-b;
+  }
+  function multiply(a, b) {
+    // we'll fill this in shortly
+    return a*b;
+  }
+  function divide(a ,b){
+    return a/b;
+  }
+  function increment(n){
+      return n+=1;
+  }
+  function decrement(n){
+      return n-=1;
+  }
+  function makeInt(n){
+    return parseInt(n, 10);
+  }
+  function parseDecimal(n){
+      parseFloat(n);
+  }
+  
